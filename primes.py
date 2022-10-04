a8c8a0430f180fc70d072d7a00ed762c9e4c9fd8
Wong, Joshua (k21090811) <joshua.1.wong@kcl.ac.uk>
2022-10-04 21:50:38 +0100

    Solution

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    num_counter = 2 #1 isn't prime
+
+    while len(list) != number_of_primes:
+        for i in range(2, num_counter):
+            if num_counter % i == 0:
+                break
+        else:
+            list.append(num_counter)
+        num_counter += 1
+        
     return list
